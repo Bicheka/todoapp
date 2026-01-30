c1476a2273e2ebc6a80b992ccf5668e07efb09ad
bicheka <davidmartinezgilp@gmail.com>
2026-01-29 21:41:32 -0500

    replaced express api with hono and bun runtime

--- services/core/database/tables/todo-lists-table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS todo_lists (
-    id SERIAL PRIMARY KEY,
-    list_title VARCHAR(255),
-    done INT,
-    total INT,
-    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
-    updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
-);
--- services/core/todo-list/todo-list-dto.ts
+++
@@ -1,9 +0,0 @@
-export interface TodoListDTO {
-    list_title: string;
-    done: number;
-    total: number;
-}
-
-export interface TodoListResponseDTO extends TodoListDTO {
-    id: number
-}
