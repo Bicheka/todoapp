54b95af4cbf08360c4f5d04eb555999604fe4735
bicheka <davidmartinezgilp@gmail.com>
2026-02-08 14:54:58 -0500

    feat: add OpenAPI tags to todo-list routes.

--- services/tudoo/src/modules/todo-list/routes.ts
+++ services/tudoo/src/modules/todo-list/routes.ts
@@ -1,8 +1,12 @@
 import { createRoute, z } from "@hono/zod-openapi";
+
+const tags = ["Todo Lists"]
+
 export const routes = {
 	getTodoLists: createRoute({
 		method: "get",
 		path: "/",
+		tags,
 		description: "get all todo lists",
 		responses: {
 			200: {
