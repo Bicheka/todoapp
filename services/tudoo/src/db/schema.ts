515e690a81f4960046a943d727120c1773bccca0
bicheka <davidmartinezgilp@gmail.com>
2026-02-09 01:04:21 -0500

    feat(db/schema): add todos table and timestamps to todo_lists.

--- services/tudoo/src/db/schema.ts
+++ services/tudoo/src/db/schema.ts
@@ -1,6 +1,31 @@
-import { integer, pgTable, text } from "drizzle-orm/pg-core";
+import { boolean, integer, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const todoLists = pgTable("todo_lists", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  title: text("title").notNull(),
+  title: text().notNull(),
+  
+  createdAt: timestamp({ withTimezone: true })
+    .notNull()
+    .defaultNow(),
+
+  updatedAt: timestamp({ withTimezone: true })
+    .notNull()
+    .defaultNow(),
+});
+
+export const todos = pgTable("todos", {
+  id: uuid().defaultRandom().primaryKey(),
+
+  title: text().notNull(),
+  description: text(),
+
+  completed: boolean().notNull().default(false),
+
+  createdAt: timestamp({ withTimezone: true })
+    .notNull()
+    .defaultNow(),
+
+  updatedAt: timestamp({ withTimezone: true })
+    .notNull()
+    .defaultNow(),
 });
